f884028fa5b89e32f345e85e18ad24875e3acfff
Adam Crowe <crowead@tcd.ie>
2024-05-13 14:50:58 +0100

    Added 'grid.__str__()' method

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from grid import Grid
 
 def main():
     grid = Grid(3,3)
+    print(grid)
 
 if __name__ == "__main__":
     main()
--- grid.py
+++ grid.py
@@ -1,9 +1,27 @@
 class Grid:
     def __init__(self, box_width, box_height):
+        # box_width & box_height determine the width/height of the boxes that subdivide the grid
         self._box_width, self._box_height = box_width, box_height
         self._length = box_width * box_height
         
+        # square template - value is zero, and can take any value from 1 to L
+        square = {
+            "value": 0
+        }
+
         # initialise grid as 2D list
-        square = {"value": 0}
         row = [square for i in range(self._length)]
         self._grid = [row for i in range(self._length)]
+
+    # acts as a way to print out our grid
+    def __str__(self):
+        string = ""  # our string we return
+
+        for i, row in enumerate(self._grid):
+            string += "\n\n" if i % self._box_height == 0 else "\n"
+            for j, square in enumerate(row):
+                string += "   " if j % self._box_width == 0 else " "
+                string += str(square["value"]) if square["value"] != 0 else "\u2610"
+        
+        string += "\n"
+        return string
